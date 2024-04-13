2aed3b30b3562bd87679bd5d34c3bfa4151986e6
ShreeramNew <shreerambca1@gmail.com>
2024-04-13 15:08:09 +0530

    trying to implement background task

--- components/home.js
+++ components/home.js
@@ -1,23 +1,49 @@
 import { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import { getBatteryLevelAsync } from "expo-battery";
+import { Audio } from 'expo-av';
+import myAudio from "../alarm-church-bell-18533.mp3";
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager'
+
+const BACKGROUND_FETCH_TASK = 'background-fetch';
+
+TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+  console.log("My task is running in background");
+  return BackgroundFetch.BackgroundFetchResult.NoData;
+});
 
 
 export default function Home() {
    let [battery, setBattery] = useState(0);
+   let [music, setMusic] = useState(null);
+
+
+   async function registerBackgroundFetchAsync() {
+      console.log("Reistered succefully");
+      return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
+        minimumInterval: 1, 
+        stopOnTerminate: false, 
+        startOnBoot: true,
+      });
+    }
+
 
    useEffect(() => {
       let updateBattery = async () => {
+         //Get current battery percentage
+
          let newPercentage = await getBatteryLevelAsync();
-         let roundedValue=Math.round(newPercentage*100)
-         console.log(newPercentage*100);
-         console.log(roundedValue);
+         let roundedValue = Math.round(newPercentage * 100); 
          setBattery(roundedValue);
       };
 
-      updateBattery();
+      updateBattery(); //Call the updateBattery for first time
 
-      const myInterVal = setInterval(updateBattery, 10000);
+      const myInterVal = setInterval(updateBattery, 10000); // Check battery in every 10 seconds
+
+   
+      registerBackgroundFetchAsync();
 
       return () => {
          clearInterval(myInterVal);
@@ -25,16 +51,45 @@ export default function Home() {
    }, []);
 
    useEffect(() => {
-      if (battery === 31) {
-         alert("Your battery reached 30%");
+      //Set a alert, when battery reaches specified percentage
+      if (battery === 28) {
+         playSound();
 
          //Alarm
       }
    }, [battery]);
 
+   let createAudio=async ()=>{
+    
+
+   }
+
+   const playSound=async ()=>{
+      //Set up and play the sound
+      const { sound } = await Audio.Sound.createAsync(myAudio);
+      setMusic(sound);
+      await sound.playAsync();
+
+   }
+   const pauseSound=async ()=>{
+      await music.pauseAsync()
+   }
+   useEffect(() => {
+      return music
+        ? () => {
+            console.log('Unloading music');
+            music.unloadAsync();
+          }
+        : undefined;
+    }, [music]);
+  
+
    return (
       <View style={myStyle.conatiner}>
          <Text style={myStyle.textStyle}>{battery}</Text>
+         <Button title="Play Sound" onPress={playSound}/>
+         <Button title="Pause Sound" onPress={pauseSound}/>
+
       </View>
    );
 }
