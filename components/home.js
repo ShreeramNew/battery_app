72750dbc5252d1c2c03d09a48fe7d921f9a3ab08
ShreeramNew <shreerambca1@gmail.com>
2024-04-14 22:20:46 +0530

    feat- trying to add background tasks

--- components/home.js
+++ components/home.js
@@ -1,49 +1,55 @@
 import { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { getBatteryLevelAsync } from "expo-battery";
-import { Audio } from 'expo-av';
+import { Audio } from "expo-av";
 import myAudio from "../alarm-church-bell-18533.mp3";
-import * as BackgroundFetch from 'expo-background-fetch';
-import * as TaskManager from 'expo-task-manager'
 
-const BACKGROUND_FETCH_TASK = 'background-fetch';
+import * as BackgroundFetch from "expo-background-fetch"
+import * as TaskManager from "expo-task-manager"
 
-TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
-  console.log("My task is running in background");
-  return BackgroundFetch.BackgroundFetchResult.NoData;
-});
+const TASK_NAME = "BACKGROUND_TASK"
+
+TaskManager.defineTask(TASK_NAME, () => {
+  
+    // fetch data here... 
+    setInterval(()=>{
+      console.log("Working each second"
+      )
+    },1000);
+    console.log("My task");
+    BackgroundFetch.BackgroundFetchResult.NewData;
+})
+
 
 
 export default function Home() {
    let [battery, setBattery] = useState(0);
    let [music, setMusic] = useState(null);
 
-
-   async function registerBackgroundFetchAsync() {
-      console.log("Reistered succefully");
-      return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
-        minimumInterval: 1, 
-        stopOnTerminate: false, 
-        startOnBoot: true,
-      });
+   const RegisterBackgroundTask = async () => {
+      try {
+        await BackgroundFetch.registerTaskAsync(TASK_NAME, {
+          minimumInterval: 5, // seconds,
+        })
+        console.log("Task registered")
+      } catch (err) {
+        console.log("Task Register failed:", err)
+      }
     }
 
-
+   
    useEffect(() => {
       let updateBattery = async () => {
          //Get current battery percentage
-
          let newPercentage = await getBatteryLevelAsync();
-         let roundedValue = Math.round(newPercentage * 100); 
+         let roundedValue = Math.round(newPercentage * 100);
          setBattery(roundedValue);
       };
 
+      RegisterBackgroundTask();
       updateBattery(); //Call the updateBattery for first time
 
       const myInterVal = setInterval(updateBattery, 10000); // Check battery in every 10 seconds
-
-   
-      registerBackgroundFetchAsync();
 
       return () => {
          clearInterval(myInterVal);
@@ -59,37 +65,30 @@ export default function Home() {
       }
    }, [battery]);
 
-   let createAudio=async ()=>{
-    
-
-   }
-
-   const playSound=async ()=>{
+   const playSound = async () => {
       //Set up and play the sound
       const { sound } = await Audio.Sound.createAsync(myAudio);
       setMusic(sound);
       await sound.playAsync();
+   };
+   const pauseSound = async () => {
+      await music.pauseAsync();
+   };
 
-   }
-   const pauseSound=async ()=>{
-      await music.pauseAsync()
-   }
    useEffect(() => {
       return music
-        ? () => {
-            console.log('Unloading music');
-            music.unloadAsync();
-          }
-        : undefined;
-    }, [music]);
-  
+         ? () => {
+              console.log("Unloading music");
+              music.unloadAsync();
+           }
+         : undefined;
+   }, [music]);
 
    return (
       <View style={myStyle.conatiner}>
          <Text style={myStyle.textStyle}>{battery}</Text>
-         <Button title="Play Sound" onPress={playSound}/>
-         <Button title="Pause Sound" onPress={pauseSound}/>
-
+         <Button title="Play Sound" onPress={playSound} />
+         <Button title="Pause Sound" onPress={pauseSound} />
       </View>
    );
 }
